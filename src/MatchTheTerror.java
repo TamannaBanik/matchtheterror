96474f5588a868204af0b9221b6b0198af7159e4
TamannaBanik <tamannabanik4@gmail.com>
2026-03-16 23:43:29 +0530

    second commit

--- src/MatchTheTerror.java
+++ src/MatchTheTerror.java
@@ -26,9 +26,12 @@ public class MatchTheTerror extends JFrame {
         initializeCards();
 
         // Setup the grid UI
+        getContentPane().setBackground(Color.BLACK);
+        
         JPanel gridPanel = new JPanel();
         gridPanel.setLayout(new GridLayout(4, 4, 10, 10));
         gridPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        gridPanel.setBackground(Color.BLACK);
 
         for (Card card : cards) {
             gridPanel.add(card);
@@ -104,19 +107,65 @@ public class MatchTheTerror extends JFrame {
         private boolean isFaceUp = false;
         private boolean isMatched = false;
         
+        // Animation fields
+        private Timer animTimer;
+        private double scaleX = 1.0;
+        private boolean isAnimating = false;
+        private int flippingHalf = 0; // 0=none, 1=shrinking, 2=expanding
+        private boolean targetFaceUp;
+        
         public Card(int pairId, String faceText, String type) {
             this.pairId = pairId;
             this.faceText = faceText;
             this.type = type;
             
             // Initial appearance (face down)
-            setText("?");
-            setFont(new Font("Arial", Font.BOLD, 30));
-            setBackground(Color.DARK_GRAY);
-            setForeground(Color.WHITE);
+            setFaceDownAppearance();
             setFocusPainted(false);
+            setContentAreaFilled(false);
+            setBorderPainted(false);
             
             addActionListener(this);
+            
+            animTimer = new Timer(15, e -> {
+                if (flippingHalf == 1) { // shrinking
+                    scaleX -= 0.1;
+                    if (scaleX <= 0.01) {
+                        scaleX = 0.01;
+                        flippingHalf = 2; // start expanding
+                        isFaceUp = targetFaceUp;
+                        if (isFaceUp) {
+                            setFaceUpAppearance();
+                        } else {
+                            setFaceDownAppearance();
+                        }
+                    }
+                } else if (flippingHalf == 2) { // expanding
+                    scaleX += 0.1;
+                    if (scaleX >= 1.0) {
+                        scaleX = 1.0;
+                        isAnimating = false;
+                        flippingHalf = 0;
+                        animTimer.stop();
+                    }
+                }
+                repaint();
+            });
+        }
+        
+        private void setFaceDownAppearance() {
+            setText("?");
+            setFont(new Font("Arial", Font.BOLD, 30));
+            setBackground(new Color(200, 160, 255)); // Light purple
+            setForeground(Color.WHITE);
+            setIcon(null);
+        }
+        
+        private void setFaceUpAppearance() {
+            setText("<html><center>" + faceText.replaceAll(" ", "<br>") + "</center></html>");
+            setFont(new Font("Arial", Font.BOLD, 30));
+            setBackground(new Color(230, 210, 255)); // Lighter purple
+            setForeground(Color.BLACK);
         }
 
         public int getPairId() {
@@ -129,36 +178,34 @@ public class MatchTheTerror extends JFrame {
 
         public void setMatched(boolean matched) {
             isMatched = matched;
-            setBackground(Color.GREEN);
+            setBackground(new Color(150, 255, 150)); // Light green
             setForeground(Color.BLACK);
             setEnabled(false); // Disable interaction once matched
         }
 
         public void flipUp() {
-            if (!isFaceUp && !isMatched) {
-                isFaceUp = true;
-                setText("<html><center>" + faceText.replaceAll(" ", "<br>") + "</center></html>");
-                setBackground(Color.LIGHT_GRAY);
-                setForeground(Color.BLACK);
-                // In a future version with images, you'd load the image here:
-                // setIcon(new ImageIcon("images/" + faceText + ".png"));
+            if (!isFaceUp && !isMatched && !isAnimating) {
+                startFlip(true);
             }
         }
 
         public void flipDown() {
             if (isFaceUp && !isMatched) {
-                isFaceUp = false;
-                setText("?");
-                setBackground(Color.DARK_GRAY);
-                setForeground(Color.WHITE);
-                setIcon(null);
+                startFlip(false);
             }
+        }
+        
+        private void startFlip(boolean faceUp) {
+            targetFaceUp = faceUp;
+            isAnimating = true;
+            flippingHalf = 1; // start shrinking
+            animTimer.start();
         }
 
         @Override
         public void actionPerformed(ActionEvent e) {
-            // Do nothing if card is already face up or game is checking a match
-            if (isFaceUp || isMatched || timer.isRunning()) {
+            // Do nothing if card is already face up or game is checking a match or currently animating
+            if (isFaceUp || isMatched || timer.isRunning() || isAnimating) {
                 return;
             }
 
@@ -171,6 +218,29 @@ public class MatchTheTerror extends JFrame {
                 // Start a short delay before checking to let user see the card
                 timer.start();
             }
+        }
+        
+        @Override
+        protected void paintComponent(Graphics g) {
+            Graphics2D g2 = (Graphics2D) g.create();
+            g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+            
+            int w = getWidth();
+            int h = getHeight();
+            
+            if (isAnimating) {
+                double sx = Math.max(scaleX, 0.01);
+                // Translate to center so it scales around its horizontal center
+                g2.translate(w / 2.0 * (1.0 - sx), 0);
+                g2.scale(sx, 1.0);
+            }
+            
+            // Draw rounded background
+            g2.setColor(getBackground());
+            g2.fillRoundRect(0, 0, w, h, 30, 30);
+            
+            super.paintComponent(g2);
+            g2.dispose();
         }
     }
 
